1fa954fbea1a599a5612e49a4f4c15e28ea05e27
Luis Mota <madbroxz@outlook.com>
2023-11-09 23:56:36 -0600

    V.6.3.5

---
+++ assets/JS/index.js
@@ -0,0 +1,45 @@
+const SR = ScrollReveal()
+
+
+SR.reveal('.container', {
+
+    duration: 2000,
+    origin: 'left',
+    distance: '300px',
+
+}) 
+
+
+SR.reveal('.calendario', {
+
+    duration: 3000,
+    origin: 'bottom',
+    distance: '300px',
+
+}) 
+
+
+SR.reveal('.Loc', {
+
+    duration: 2000,
+    origin: 'top',
+    distance: '300px',
+
+}) 
+
+
+SR.reveal('.tec-logos', {
+
+    duration: 2000,
+    origin: 'right',
+    distance: '300px',
+
+}) 
+
+SR.reveal('.Servicios', {
+
+    duration: 3000,
+    origin: 'bottom',
+    distance: '300px',
+
+}) 
